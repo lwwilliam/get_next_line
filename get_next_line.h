4271122192359a82befaa7e68185988b542259ac
Lee William <lwilliam@u82z04s02.42kl.edu.my>
2022-08-12 10:22:02 +0800

    gnl

--- get_next_line.h
+++ get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: lwilliam <lwilliam@student.42kl.edu.my>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/03 17:55:32 by lwilliam          #+#    #+#             */
-/*   Updated: 2022/07/08 11:56:51 by lwilliam         ###   ########.fr       */
+/*   Updated: 2022/08/04 10:35:43 by lwilliam         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,7 @@
 # include <unistd.h>
 # include <stdlib.h>
 # include <stdio.h>
+#include <fcntl.h>
 
 char	*get_next_line(int fd);
 char	*ft_strdup(const char *s);
@@ -23,5 +24,6 @@ char	*ft_strchr(const char *str, int y);
 size_t	ft_strlen(const char *str);
 char	*ft_substr(char const *s, unsigned int start, size_t len);
 char	*ft_strjoin(char const *s1, char const *s2);
+int		read_funct(int fd, char **buffer, int *x);
 
 #endif
